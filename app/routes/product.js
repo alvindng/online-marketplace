24dd58681d71945d1f8bcc230fbd8525ce9ac913
Alvin Ng and Marty Fitchen <alvin@>
2016-08-24 10:39:32 -0700

    add working files allowing users to add items to cart whether on the store route or product dynamic route

---
+++ app/routes/product.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service(),
+  model(params) {
+    return this.store.findRecord('product', params.product_id);
+  },
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    }
+  }
+});
