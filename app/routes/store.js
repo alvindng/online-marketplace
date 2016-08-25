5bbf5fd5f858d27b7a416d3c955894c15c7b9735
Alvin Ng and Marty Fitchen <alvin@>
2016-08-25 16:46:02 -0700

    added firebase query to sort the products by price

--- app/routes/store.js
+++ app/routes/store.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-   return this.store.findAll('product');
- },
+  model: function() {
+    return this.store.query('product', {
+      orderBy: 'cost',
+    });
+  }
 });
