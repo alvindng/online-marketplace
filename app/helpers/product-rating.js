a326f435982ae33607ca1560c8a8864e3b935125
Alvin Ng and Marty Fitchen <marty@>
2016-08-25 11:41:39 -0700

    add helper to display ratings by star glyphicons

---
+++ app/helpers/product-rating.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export function productRating(params) {
+  var productRating = params[0].get('rating');
+
+  if (productRating === 1) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span>');
+  } else if (productRating === 2){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
+  } else if (productRating === 3){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
+  } else if (productRating === 4){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
+  } else if (productRating === 5){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
+  }
+}
+
+export default Ember.Helper.helper(productRating);
---
+++ tests/unit/helpers/product-rating-test.js
@@ -0,0 +1,10 @@
+import { productRating } from 'online-marketplace/helpers/product-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | product rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = productRating([42]);
+  assert.ok(result);
+});
